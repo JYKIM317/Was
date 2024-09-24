aa46c0b6218963df3f2201b4269d2ad6249b1980
tongticktack <>
2024-09-24 18:18:05 +0900

    feat: http(요청, 응답) DTO 클래스 선언

---
+++ src/dto/Request.ts
@@ -0,0 +1,29 @@
+export class Request {
+    headers: {[key: string]: string} = {};
+    body: string;
+    method: string;
+    path: string;
+    version: string;
+    constructor(msg) {
+        this.parseMsg(msg);
+    }
+
+    private parseMsg(msg) {
+        const [headerMsg, bodyMsg] = msg.split("\r\n\r\n");
+        const [startLine, ...requestHeader] = headerMsg.split("\r\n");
+        this.body = bodyMsg;
+        this.parseStartLine(startLine);
+        this.parseHeader(requestHeader);
+    }
+
+    private parseStartLine(startLine) {
+        [this.method, this.path, this.version] = startLine.split(' ');
+    }
+
+    private parseHeader(headerMsg) {
+        headerMsg.forEach((line) => {
+            const [key, value] = line.split(":");
+            this.headers[key] = value.trim();
+        });
+    }
+}
