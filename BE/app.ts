0d33eb4cc46088d258d3e9ea23042bd865894001
tongticktack <>
2024-09-26 19:08:42 +0900

    feat(BE): userRouter 생성 및 routerStack 적용

--- BE/app.ts
+++ BE/app.ts
@@ -1,16 +1,27 @@
 import net from 'net';
 import { logger } from './logger';
 import { Request } from './dto/Request';
+import { routeStack } from './route/RouteStack';
 import { staticRouter } from './route/staticRouter';
+import { userRouter } from './route/userRouter';
+import { notFoundResponse } from './util/response';
+
+routeStack.use("/", staticRouter);
+routeStack.use("/user", userRouter);
 
 const server = net.createServer(socket => {
     socket.on("data", (data) => {
         const socketData = data.toString();
         const request =  new Request(socketData);
-        console.log(request);
-        const response = staticRouter.requestHandler(request);
-       
-        socket.write(response.responseMsg);
+        const router = routeStack.find(request.path);
+
+        if(router) {
+            const response = router.requestHandler(request);
+            socket.write(response.responseMsg);
+        } else {
+            socket.write(notFoundResponse(request).responseMsg);
+        }
+
         if(request.headers.Connection == null) socket.end();
     });
 });
