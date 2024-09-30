eab4cb2648d86633e9cd262fdf61e331bb6d8036
JYKIM317 <augu202@naver.com>
2024-09-30 21:10:33 +0900

    feat: winston으로 log 파일에 HTTP Request 로깅하도록 추가

--- BE/logger.ts
+++ BE/logger.ts
@@ -1,10 +1,18 @@
 import winston from 'winston';
+import winstonDaily from 'winston-daily-rotate-file';
 
 const logger = winston.createLogger({
     level: 'debug',
     format: winston.format.json(),
     transports: [
-        new winston.transports.Console()
+        new winstonDaily({
+            level: 'debug',
+            datePattern: 'YYYY-MM-DD',
+            dirname: `./logs`,
+            filename: `%DATE%.log`,
+            maxFiles: 3,
+            zippedArchive: false
+        })
     ]
 });
 
