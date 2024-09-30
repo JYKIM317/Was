80ecd9e7fa7ce4d5185861d9796626aef95db9f7
JYKIM317 <augu202@naver.com>
2024-09-30 20:43:09 +0900

    feat: 프론트 Register Fetch 메소드를 GET -> POST로 바꾸고 새롭게 빌드

--- FE/src/layouts/Register.tsx
+++ FE/src/layouts/Register.tsx
@@ -7,7 +7,7 @@ const baseURL = import.meta.env.VITE_BE_BASE_URL;
 
 
 
-const Register : React.FC = () => {
+const Register: React.FC = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
@@ -17,25 +17,27 @@ const Register : React.FC = () => {
     navigate('/login');
   };
 
-  const fetchRegister  = async () => {
-    await fetch(`${baseURL}/user/register?email=${email}&password=${password}&name=${name}`, {
-        method: "GET"
+  const fetchRegister = async () => {
+    await fetch(`${baseURL}/user/register`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password, name })
     });
   }
 
   return (
     <>
       <Navigation title="HELLO, WEB!">
-          <Button text="로그인/회원가입" size="small" onClick={navigateToLogin} />
+        <Button text="로그인/회원가입" size="small" onClick={navigateToLogin} />
       </Navigation>
       <Information title="회원가입" />
       <HugFrame>
-          <InputBox label="이메일" type="email" placeholder="이메일을 입력해주세요" value={email} onChange={(e) => setEmail(e.target.value)} />
-          <InputBox label="닉네임" type="text" placeholder="닉네임을 입력해주세요" value={name} onChange={(e) => setName(e.target.value)} />
-          <InputBox label="비밀번호" type="password" placeholder="비밀번호를 입력해주세요" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <InputBox label="이메일" type="email" placeholder="이메일을 입력해주세요" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <InputBox label="닉네임" type="text" placeholder="닉네임을 입력해주세요" value={name} onChange={(e) => setName(e.target.value)} />
+        <InputBox label="비밀번호" type="password" placeholder="비밀번호를 입력해주세요" value={password} onChange={(e) => setPassword(e.target.value)} />
       </HugFrame>
       <HugFrame>
-          <Button text="회원가입" size="large" onClick={fetchRegister} />
+        <Button text="회원가입" size="large" onClick={fetchRegister} />
       </HugFrame>
     </>
   )
