01b5a7ef1683240b836604c2aba4899c2b23b348
JYKIM317 <augu202@naver.com>
2024-10-01 23:44:14 +0900

    remove: 필요 없어진 함수 및 파일 제거

--- BE/util/response.ts
+++
@@ -1,7 +0,0 @@
-import { Response } from "../core/http/Response";
-
-function notFoundResponse(req): Response {
-    return new Response(404, req.headers.Connection ?? "close");
-}
-
-export { notFoundResponse }
