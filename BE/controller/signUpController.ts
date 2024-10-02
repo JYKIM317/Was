2257b6c6d46c65fdeadc3479ca3e0e9023c8de85
JYKIM317 <augu202@naver.com>
2024-10-02 22:26:09 +0900

    comment: TODO annotation 추가

--- BE/controller/signUpController.ts
+++ BE/controller/signUpController.ts
@@ -11,6 +11,8 @@ function signUpController(req, res) {
     const userData: signUpInfo = req.body as signUpInfo;
     const [email, password, name] = [userData.email, md5Encryption(userData.password), userData.name];
 
+    //TODO: email 정규표현식 검사 추가
+    //
     try {
         UserRepository.getUser(email).then((response) => {
             const result = response[0][0];
