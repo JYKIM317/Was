1233d4a8be5840f197671e7eae4df68be60ab7e7
JYKIM317 <augu202@naver.com>
2024-10-07 23:30:34 +0900

    refactor: 기존 기능 스크립트 추가

--- static/layouts/loginLayout.js
+++ static/layouts/loginLayout.js
@@ -2,40 +2,56 @@ import { LargeButton, SmallButton } from "../components/Button.js";
 import { InputBox } from "../components/InputBox.js";
 import { Navigation, Information, HugFrame } from "../components/Frame.js";
 
-/*
-    const fetchLogin = async () => {
-        await fetch(`${baseURL}/user/login`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ email, password })
-        });
-    }
-*/
+function render() {
+    const navigationNode = document
+        .createRange()
+        .createContextualFragment(Navigation("HELLO, WEB!", [SmallButton("로그인/회원가입", "user-navigator-button")]));
 
-const fragment = document.createDocumentFragment();
-
-const navigationNode = document
-    .createRange()
-    .createContextualFragment(Navigation("HELLO, WEB!", [SmallButton("로그인/회원가입", "login-navigator-button")]));
-fragment.appendChild(navigationNode);
-
-const informationNode = document.createRange().createContextualFragment(Information("로그인"));
-fragment.appendChild(informationNode);
-
-const loginInputFrameNode = document.createRange().createContextualFragment(HugFrame("login-frame", [
-    InputBox("input-email", "이메일", "email", "이메일을 입력해주세요"),
-    InputBox("input-password", "비밀번호", "password", "비밀번호를 입력해주세요")
-]));
-fragment.appendChild(loginInputFrameNode);
-
-const registerRouteText = `<span className='signup-info'>
+    const informationNode = document.createRange().createContextualFragment(Information("로그인"));
+    const loginInputFrameNode = document.createRange().createContextualFragment(HugFrame("login-frame", [
+        InputBox("input-email", "이메일", "email", "이메일을 입력해주세요"),
+        InputBox("input-password", "비밀번호", "password", "비밀번호를 입력해주세요")
+    ]));
+    const registerRouteText = `<span className='signup-info'>
                     아직 회원가입을 안하셨나요?
                     <a href="http://localhost:8080/register.html" className="text-link"> 회원가입하기</a>
                 </span>`;
-const loginButtonFrameNode = document.createRange().createContextualFragment(HugFrame("login-frame", [
-    LargeButton("로그인", "login-button"),
-    registerRouteText
-]));
-fragment.appendChild(loginButtonFrameNode);
+    const loginButtonFrameNode = document.createRange().createContextualFragment(HugFrame("login-frame", [
+        LargeButton("로그인", "login-button"),
+        registerRouteText
+    ]));
 
-document.body.querySelector("#root").appendChild(fragment);
+    const fragment = document.createDocumentFragment();
+    fragment.appendChild(navigationNode);
+    fragment.appendChild(informationNode);
+    fragment.appendChild(loginInputFrameNode);
+    fragment.appendChild(loginButtonFrameNode);
+
+    document.body.querySelector("#root").appendChild(fragment);
+}
+
+function addEvent() {
+    const url = "http://localhost:8080";
+
+    //login fetch
+    document.getElementById("login-button").addEventListener("click", async (event) => {
+        const email = document.getElementById("input-email").querySelector("input").value.trim();
+        const password = document.getElementById("input-password").querySelector("input").value.trim();
+
+        if (email !== "" && password !== "") {
+            await fetch(`${baseURL}/user/login`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ email, password })
+            });
+        }
+    });
+
+    //navigate to register page
+    document.getElementById("user-navigator-button").addEventListener("click", (event) => {
+        window.location.href = `${url}/register.html`;
+    });
+}
+
+render();
+addEvent();
--- static/layouts/mainLayout.js
+++ static/layouts/mainLayout.js
@@ -1,14 +1,28 @@
 import { SmallButton } from "../components/Button.js";
 import { Navigation, Information } from "../components/Frame.js";
 
-const fragment = document.createDocumentFragment();
+function render() {
+    const navigationNode = document
+        .createRange()
+        .createContextualFragment(Navigation("HELLO, WEB!", [SmallButton("로그인/회원가입", "user-navigator-button")]));
+    const informationNode = document.createRange().createContextualFragment(Information("부스트캠프 백엔드 교육용 페이지", "main-information", ["<h2>HELLO, WEB! 입니다.</h2>"]));
 
-const navigationNode = document
-    .createRange()
-    .createContextualFragment(Navigation("HELLO, WEB!", [SmallButton("로그인/회원가입", "login-navigator-button")]));
-fragment.appendChild(navigationNode);
+    const fragment = document.createDocumentFragment();
+    fragment.appendChild(navigationNode);
+    fragment.appendChild(informationNode);
 
-const informationNode = document.createRange().createContextualFragment(Information("부스트캠프 백엔드 교육용 페이지", "main-information", ["<h2>HELLO, WEB! 입니다.</h2>"]));
-fragment.appendChild(informationNode);
+    document.body.querySelector("#root").appendChild(fragment);
+}
 
-document.body.querySelector("#root").appendChild(fragment);
+
+function addEvent() {
+    const url = "http://localhost:8080";
+
+    //navigate to login page
+    document.getElementById("user-navigator-button").addEventListener("click", (event) => {
+        window.location.href = `${url}/login.html`;
+    });
+}
+
+render();
+addEvent();
