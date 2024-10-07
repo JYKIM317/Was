f06ea9b36f2c7a1150b08cbd876c85d13d6b3693
JYKIM317 <augu202@naver.com>
2024-10-07 22:51:46 +0900

    fix: Content-Length 불일치 문제 해결

--- BE/core/http/Response.ts
+++ BE/core/http/Response.ts
@@ -90,10 +90,10 @@ export class Response {
         return header;
     }
 
-    private socketWrite(header, body?) {
+    private socketWrite(header, body = "\r\n") {
         if (!this.statusCode) throw new Error("Status code has not been set yet.");
         const startLine = `HTTP/1.1 ${this.statusCode} ${statusMsg[this.statusCode]}\r\n`;
-        const emptyLine = "\r\n\r\n";
+        const emptyLine = "\r\n";
 
         this.socket.write(startLine);
         this.socket.write(header);
