5733b5adfca9d33ca920b2817b59092a5861471d
JYKIM317 <augu202@naver.com>
2024-10-09 17:06:04 +0900

    feat: 클라이언트 토큰 검증 요청 함수 작성

---
+++ static/scripts/authorization.js
@@ -0,0 +1,28 @@
+import { fetchPOST } from "./fetch.js";
+
+const url = "http://localhost:8080";
+
+async function verifyAccessTokenValid() {
+    const accessToken = window.localStorage.getItem("accessToken");
+    if (accessToken == null) return false;
+
+    return await fetchPOST(`${url}/autorization/verify`, { email, password }).then(async (response) => {
+        const isOK = 200;
+        const UNATHORIZED = 401;
+        if (response.status === isOK) {
+            return true;
+        }
+        else if (response.status === UNATHORIZED) {
+            return await requestTokenRefresh();
+        } else {
+            return false;
+        }
+    });
+}
+
+async function requestTokenRefresh() {
+    const refreshToken = window.localStorage.getItem("refreshToken");
+
+}
+
+export { verifyAccessTokenValid }
