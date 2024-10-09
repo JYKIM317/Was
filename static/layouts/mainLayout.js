46da7d166ed49326de6556fca0eadcd12fd98f14
JYKIM317 <augu202@naver.com>
2024-10-09 20:28:41 +0900

    feat: 로그인 상태 메인 페이지 네비게이터 반영

--- static/layouts/mainLayout.js
+++ static/layouts/mainLayout.js
@@ -1,10 +1,21 @@
 import { SmallButton } from "../components/Button.js";
-import { Navigation, Information } from "../components/Frame.js";
+import { Navigation, Information, HorizontalHugFrame } from "../components/Frame.js";
+import { verifyAccessTokenValid } from "../scripts/authorization.js";
 
-function render() {
+async function render() {
+    const tokenValid = await verifyAccessTokenValid();
+
     const navigationNode = document
         .createRange()
-        .createContextualFragment(Navigation("HELLO, WEB!", [SmallButton("로그인/회원가입", "user-navigator-button")]));
+        .createContextualFragment(Navigation("HELLO, WEB!",
+            tokenValid
+                ? [HorizontalHugFrame("user-naviator-button-list", [
+                    SmallButton("멤버리스트", "user-memberlist-button"),
+                    SmallButton("마이페이지", "user-mypage-button"),
+                    SmallButton("로그아웃", "user-logout-button")
+                ])]
+                : [SmallButton("로그인/회원가입", "user-navigator-button")]
+        ));
     const informationNode = document.createRange().createContextualFragment(Information("부스트캠프 백엔드 교육용 페이지", "main-information", ["<h2>HELLO, WEB! 입니다.</h2>"]));
 
     const fragment = document.createDocumentFragment();
@@ -12,6 +23,7 @@ function render() {
     fragment.appendChild(informationNode);
 
     document.body.querySelector("#root").appendChild(fragment);
+    addEvent();
 }
 
 
@@ -22,7 +34,8 @@ function addEvent() {
     document.getElementById("user-navigator-button").addEventListener("click", (event) => {
         window.location.href = `${url}/login.html`;
     });
+
+    //TODO: 로그인 상태 네비게이터 버튼 이벤트 등록
 }
 
 render();
-addEvent();
