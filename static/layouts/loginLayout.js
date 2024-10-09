b1c82672a75f897bf66b0a3e44d3a9500780531a
JYKIM317 <augu202@naver.com>
2024-10-09 19:00:16 +0900

    fix: 로그인 실패 시 json이 null로 반환 돼 redirect를 읽을 수 없는 문제 해결

--- static/layouts/loginLayout.js
+++ static/layouts/loginLayout.js
@@ -42,7 +42,9 @@ function addEvent() {
         if (email !== "" && password !== "") {
             fetchPOST(`${url}/user/login`, { email, password }).then((response) => {
                 const isOK = 200;
+                console.log(response.body);
                 if (response.status === isOK) return response.json();
+                else return {};
             }).then((json) => {
                 if (json.redirect != null) window.location.href = json.redirect;
             });
