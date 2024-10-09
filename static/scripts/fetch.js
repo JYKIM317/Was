878f7ac26137f0eb00ebe0c2b61fbf4ddb51ff94
JYKIM317 <augu202@naver.com>
2024-10-09 14:51:24 +0900

    refactor: response가 반환되도록 return을 추가했고, json 전달 과정에서 Promise 결과를 반환할 수 있도록 await 추가

--- static/scripts/fetch.js
+++ static/scripts/fetch.js
@@ -2,14 +2,14 @@ async function fetchPOST(uri, data) {
     const accessToken = window.localStorage.getItem("accessToken");
     if (accessToken != null) data.accessToken = accessToken;
 
-    await fetch(uri, {
+    return await fetch(uri, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data)
-    }).then((response) => {
+    }).then(async (response) => {
         const contentType = response.headers.get("Content-Type");
         const isJSON = contentType === "application/json";
-        return [response, isJSON ? response.json() : {}];
+        return [response, isJSON ? await response.json() : {}];
     }).then(([response, body]) => {
         if (body.accessToken != null) {
             window.localStorage.setItem("accessToken", body.accessToken);
