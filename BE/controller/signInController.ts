3afdc6ddd6a414b199036718d44ac4fba534dcdc
JYKIM317 <augu202@naver.com>
2024-10-09 11:53:58 +0900

    feat: 로그인 성공 시 Access Token과 Refresh Token을 함께 전달하도록 변경

--- BE/controller/signInController.ts
+++ BE/controller/signInController.ts
@@ -1,8 +1,8 @@
-import { session } from "../core/session/Session";
+
 import { logger } from "../logger";
 import { UserRepository } from "../repository/UserRepository";
 import { md5Encryption } from "../util/crypto";
-import { sha1Encryption } from "../util/crypto";
+import Authorization from "../core/auth/Authorization"
 
 type signInInfo = {
     email: string,
@@ -13,22 +13,17 @@ function signInController(req, res) {
     const userData: signInInfo = req.body as signInInfo;
     const { email, password } = userData;
     const encryptionPW = md5Encryption(password);
-    const sid = req.headers.cookie != null ? req.headers.cookie.sid : "none";
 
-    if (session.isExist(sid)) res.setStatus(200).send(session.get(sid));
-    else try {
+    try {
         UserRepository.getUser(email).then((response) => {
             const result = response[0][0];
             const userExist = result != null;
-            const DAY = 60 * 60 * 24;
-
             if (!userExist) return res.setStatus(400).send();
             if (encryptionPW !== result.password) return res.setStatus(400).send();
 
-            const sid = sha1Encryption(email + Date.now().toString());
-            session.set(sid, result.id);
-            res.setCookie("sid", sid, { HttpOnly: true, Path: "/", "Max-Age": 30 * DAY });
-            res.setStatus(200).json({ redirect: "/" });
+            const accessToken = Authorization.generateToken("Access");
+            const refreshToken = Authorization.generateToken("Refresh");
+            res.setStatus(200).json({ redirect: "/", accessToken, refreshToken });
         });
     } catch (e) {
         logger.error(e);
