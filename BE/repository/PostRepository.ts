337401e03991497a9df836e332a1d3d65e72a466
JYKIM317 <augu202@naver.com>
2024-10-14 21:26:46 +0900

    refactor: 게시판 페이지 오프셋 이름 변경

--- BE/repository/PostRepository.ts
+++ BE/repository/PostRepository.ts
@@ -4,7 +4,7 @@ class PostRepository {
     static TABLE_NAME = "post";
 
     static async getBoard(page) {
-        const SELECT_PAGE = page * 10 - 10;
+        const SELECT_PAGE_OFFSET = page * 10 - 10;
 
         return await DBManager.select({
             table: this.TABLE_NAME,
@@ -12,7 +12,7 @@ class PostRepository {
             orderBy: "id",
             desc: true,
             limit: 10,
-            offset: SELECT_PAGE
+            offset: SELECT_PAGE_OFFSET
         });
     }
 
