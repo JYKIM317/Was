479783ea376386ca0e3aa227255a2a488579740d
JYKIM317 <augu202@naver.com>
2024-10-14 18:59:32 +0900

    refactor: 파일 위치 변경으로 인한 경로 변경

--- BE/controller/static/staticController.ts
+++ BE/controller/static/staticController.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const filePath = fileURLToPath(import.meta.url);
-const staticFilePath = path.join(filePath, "../../../", "static");
+const staticFilePath = path.join(filePath, "../../../../", "static");
 
 function staticController(req, res) {
     const filePath = path.join(staticFilePath, req.path === '/' ? 'index.html' : req.path);
