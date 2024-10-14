2b56f5f592a4344f5c81dde7e5da3d69fd2c2d26
JYKIM317 <augu202@naver.com>
2024-10-14 21:24:58 +0900

    feat: 게시판 글 응답하는 함수 작성

---
+++ BE/controller/board/postController.ts
@@ -0,0 +1,22 @@
+import { PostRepository } from "../../repository/PostRepository"
+import { logger } from "../../logger";
+
+async function boardController(req, res) {
+    try {
+        const page = req.query.p;
+        PostRepository.getBoard(page).then((response) => {
+            const result = response[0];
+            res.setStatus(200).json({ result });
+        });
+    } catch (e) {
+        logger.error(e);
+        res.setStatus(500).send();
+    }
+}
+
+async function postController(req, res) {
+
+}
+
+
+export { boardController, postController }
