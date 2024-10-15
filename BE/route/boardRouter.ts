503ff7c5cf7166163800d089532bad816ca71c3b
JYKIM317 <augu202@naver.com>
2024-10-15 21:18:02 +0900

    feat: 글 작성 로직 라우터 등록

--- BE/route/boardRouter.ts
+++ BE/route/boardRouter.ts
@@ -1,10 +1,12 @@
 import { boardController, postController } from '../controller/board/postController';
+import { postWriteController } from '../controller/board/postWriteController'
 import { commentController } from '../controller/board/commentController';
 import { Router } from '../core/router/Router';
 
 const boardRouter = new Router();
 
 boardRouter.get("/board/post", boardController);
+boardRouter.post("/board/post", postWriteController);
 boardRouter.post("/board/post/:postId", postController);
 boardRouter.post("/board/comment/:postId", commentController);
 
