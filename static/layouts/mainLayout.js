c47ea5eab75a02d3c558eb276d32840432171a03
JYKIM317 <augu202@naver.com>
2024-10-15 16:07:42 +0900

    feat: 프론트 메인 페이지 글 쓰기 버튼 이벤트 추가

--- static/layouts/mainLayout.js
+++ static/layouts/mainLayout.js
@@ -3,7 +3,7 @@ import { Navigation, Information, HorizontalHugFrame } from "../components/Frame
 import { verifyAccessTokenValid } from "../scripts/authorization.js";
 import { getBoardPage } from "../scripts/board.js";
 import { Board, PostTable, PostElement, BoardNavigation } from "../components/Board.js"
-import { fetchPOST } from "../scripts/fetch.js";
+import { fetchGET, fetchPOST } from "../scripts/fetch.js";
 
 async function render() {
     const tokenValid = await verifyAccessTokenValid();
@@ -75,7 +75,7 @@ function addEvent(isLogin) {
     document.querySelector(".post-table").addEventListener("click", async (event) => {
         const targetElement = event.target.closest(".post-element");
         if (targetElement.id !== "table-head") {
-            await fetchPOST(url + `/board/post/${targetElement.id}`).then((response) => {
+            fetchPOST(url + `/board/post/${targetElement.id}`).then((response) => {
                 const isOK = 200;
                 if (response.status === isOK) return response.json();
                 else return {};
@@ -83,6 +83,14 @@ function addEvent(isLogin) {
                 if (json.redirect != null) location.href = json.redirect;
             });
         }
+    });
+
+    document.getElementById("write-button").addEventListener("click", (_) => {
+        fetchGET(url + "/write.html").then((response) => {
+            return response.json();
+        }).then((json) => {
+            if (json.redirect != null) location.href = json.redirect;
+        });
     });
 }
 
