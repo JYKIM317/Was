66f39cc26bd858c28fb04b98b859cbc918f05b1b
JYKIM317 <augu202@naver.com>
2024-10-16 01:30:03 +0900

    feat: 글 상세 페이지 컴포넌트 구현

--- static/components/Post.js
+++ static/components/Post.js
@@ -1,3 +1,17 @@
+const PostInfo = (author, createAt, viewCount) => {
+    return `<div class="post-info">
+        <span>작성자: ${author}</span>
+        <span>작성일자: ${createAt}</span>
+        <span>조회: ${viewCount}</span>
+    </div>`;
+}
+
+const PostContent = (content) => {
+    return `<div class="post-content">
+        ${content}
+    </div>`;
+}
+
 const PostNavigation = (id, children = []) => {
     const childrenNode = children.join("\n");
     return `<div class="post-navigation" id="${id}">
@@ -5,4 +19,4 @@ const PostNavigation = (id, children = []) => {
     </div>`;
 }
 
-export { PostNavigation }
+export { PostInfo, PostContent, PostNavigation }
